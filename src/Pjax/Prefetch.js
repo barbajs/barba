d4d635e114018154d6baf514dbf224397142ed1f
Luigi De Rosa <lurukee@gmail.com>
2016-01-16 18:50:58 +0100

    Disable test prefetch

--- src/Pjax/Prefetch.js
+++ src/Pjax/Prefetch.js
@@ -8,9 +8,9 @@ var Pjax = require('./Pjax');
 var Prefetch = {
   init: function() {
     //document.body.addEventListener('mouseenter', this.onLinkEnter.bind(this));
-    
+
     //TODO IN VANILLA with mouseover event...
-    $(document).on('mouseenter', 'a', this.onLinkEnter.bind(this));
+    //$(document).on('mouseenter', 'a', this.onLinkEnter.bind(this));
   },
 
   onLinkEnter: function(evt) {
@@ -19,9 +19,9 @@ var Prefetch = {
     while (el && !el.href) {
       el = el.parentNode;
     }
-    
+
     var url = el.href;
-    
+
     //Check if the link is elegible for Pjax
     if (Pjax.preventCheck(evt, el) && !Pjax.Cache.get(url)) {
       var xhr = Utils.xhr(url);
