d62f94c1270dfe455da80336d365c6c8db2ff294
Luigi De Rosa <lurukee@gmail.com>
2016-02-01 22:54:57 +0100

    Continue adding information to the website

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+Barba.Pjax.init();
+Barba.Prefetch.init();
+
+// Add the active class when click on the menu item.
+var menu = document.querySelector('.menu');
+var items = menu.querySelectorAll('li');
+
+Barba.Dispatcher.on('linkClicked', function(el) {
+  if (!menu.contains(el)) {
+    return;
+  }
+
+  [].forEach.call(items, function(item) {
+    item.classList.remove('active');
+  });
+
+  el.parentElement.classList.add('active');
+});
