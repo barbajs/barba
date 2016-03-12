a2a25150999ce124632c4e255cfaf8b58e0bcfaa
Luigi De Rosa <lurukee@gmail.com>
2016-03-12 18:58:19 +0100

    Build 0.0.5

--- dist/barba.js
+++ dist/barba.js
@@ -61,7 +61,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	}
 	
 	var Barba = {
-	  version: '0.0.4',
+	  version: '0.0.5',
 	  Dispatcher: __webpack_require__(4),
 	  HistoryManager: __webpack_require__(5),
 	  BaseTransition: __webpack_require__(6),
@@ -1063,10 +1063,11 @@ return /******/ (function(modules) { // webpackBootstrap
 	    if (this.History.currentStatus().url === newUrl)
 	      return false;
 	
+	    this.History.add(newUrl);
+	
 	    var newContainer = this.load(newUrl);
 	    var transition = Object.create(this.getTransition());
 	
-	    this.History.add(newUrl);
 	    this.transitionProgress = true;
 	
 	    Dispatcher.trigger('initStateChange',
