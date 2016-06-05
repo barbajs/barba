5969da5230a7959c534dacf6ccd041b4f27d3db9
Luigi De Rosa <lurukee@gmail.com>
2016-06-05 16:10:49 +0200

    Increment version

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ if (typeof Promise !== 'function') {
 }
 
 var Barba = {
-  version: '0.0.8',
+  version: '0.0.9',
   Dispatcher: require('./Dispatcher/Dispatcher'),
   HistoryManager: require('./Pjax/HistoryManager'),
   BaseTransition: require('./Transition/BaseTransition'),
