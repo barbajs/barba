b2f947bbb6d110151c5a774bcd43cccef0a2371a
Luigi De Rosa <lurukee@gmail.com>
2016-06-18 21:05:52 +0200

    Use new version

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ if (typeof Promise !== 'function') {
 }
 
 var Barba = {
-  version: '0.0.9',
+  version: '0.0.10',
   Dispatcher: require('./Dispatcher/Dispatcher'),
   HistoryManager: require('./Pjax/HistoryManager'),
   BaseTransition: require('./Transition/BaseTransition'),
