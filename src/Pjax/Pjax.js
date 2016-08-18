6b617f805d49dbd9f646f0a090caf708114cf1e4
Nguyen Van Anh <anhskohbo@gmail.com>
2016-08-18 10:20:38 +0700

    Add MouseEvent in linkClicked
    Sometime, we need this event on linkClicked.

--- src/Pjax/Pjax.js
+++ src/Pjax/Pjax.js
@@ -53,7 +53,7 @@ var Pjax = {
   start: function() {
     this.init();
   },
-
+evt
   /**
    * Init the events
    *
@@ -191,7 +191,7 @@ var Pjax = {
       evt.stopPropagation();
       evt.preventDefault();
 
-      Dispatcher.trigger('linkClicked', el);
+      Dispatcher.trigger('linkClicked', el, evt);
       this.goTo(el.href);
     }
   },
