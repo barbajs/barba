27e3323599628632947b5b3aa239dbad3d864557
Thierry Michel <thmichel@gmail.com>
2019-06-04 11:54:17 +0200

    :sparkles: add EJS support for JS components

--- themes/barba/layout/components/showcase/ShowcaseGrid.js
+++ themes/barba/layout/components/showcase/ShowcaseGrid.js
@@ -1,32 +1,40 @@
 import { Component } from 'kapla';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
-// Pseudo
-// const template = require("ejs-loader!./themes/barba/layout/components/showcase/showcase-item.ejs");
+
+import template from 'ejs-compiled-loader!./showcase-item.ejs';
 
 export default class extends Component {
   load() {
-    // Your web app's Firebase configuration
-    const firebaseConfig = {
-      apiKey: "AIzaSyADgPf9rpJamT3hDg5P9UaCYrFWfxXAHps",
-      authDomain: "barba-v2.firebaseapp.com",
-      databaseURL: "https://barba-v2.firebaseio.com",
-      projectId: "barba-v2",
-      storageBucket: "barba-v2.appspot.com",
-      messagingSenderId: "1045458665835",
-      appId: "1:1045458665835:web:ae10b06bf726c27f"
-    };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
+    const data = { post: { name: 'Thierry' } };
+    console.info('ShowcaseGrid:load');
 
-    const db = firebase.firestore();
+    const html = template(data);
 
-    // Pseudo
-    db.collection('showcases').get().then((snapshot) => {
-      snapshot.docs.forEach(doc => {
-        console.log(doc.data());
-        // template.parse(doc.data());
-      });
-    });
+    console.info('RESULT', html);
+    this.$el.innerHTML = html;
+    // console.info('TEST', template()(data));
+    // // Your web app's Firebase configuration
+    // const firebaseConfig = {
+    //   apiKey: "AIzaSyADgPf9rpJamT3hDg5P9UaCYrFWfxXAHps",
+    //   authDomain: "barba-v2.firebaseapp.com",
+    //   databaseURL: "https://barba-v2.firebaseio.com",
+    //   projectId: "barba-v2",
+    //   storageBucket: "barba-v2.appspot.com",
+    //   messagingSenderId: "1045458665835",
+    //   appId: "1:1045458665835:web:ae10b06bf726c27f"
+    // };
+    // // Initialize Firebase
+    // firebase.initializeApp(firebaseConfig);
+
+    // const db = firebase.firestore();
+
+    // // Pseudo
+    // db.collection('showcases').get().then((snapshot) => {
+    //   snapshot.docs.forEach(doc => {
+    //     console.log(doc.data());
+    //     // template.parse(doc.data());
+    //   });
+    // });
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,43 @@
 module.exports = {
-  entry: [
-    'themes/barba/source/js/main.js',
-  ],
+  entry: ['themes/barba/source/js/main.js'],
   module: {
-    loaders: [{
-      test: /\.ejs$/,
-      loader: 'ejs-loader',
-    },
-    {
-      test: /\.js/,
-      use: [
-        {
-          loader: 'babel-loader',
-          options: {
-            babelrc: false,
-            presets: [
-              ['@babel/preset-env', {
-                debug: false,
-                modules: false,
-                targets: {
-                  browsers: [
-                    'last 2 Chrome versions', 'not Chrome < 60',
-                    'last 2 Safari versions', 'not Safari < 10.1',
-                    'last 2 iOS versions', 'not iOS < 10.3',
-                    'last 2 Firefox versions', 'not Firefox < 54',
-                    'last 2 Edge versions', 'not Edge < 15',
-                  ],
-                },
-                useBuiltIns: false,
-              }],
-            ],
-            plugins: ['@babel/plugin-proposal-class-properties'],
+    loaders: [
+      {
+        test: /\.js/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              babelrc: false,
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    debug: false,
+                    modules: false,
+                    targets: {
+                      browsers: [
+                        'last 2 Chrome versions',
+                        'not Chrome < 60',
+                        'last 2 Safari versions',
+                        'not Safari < 10.1',
+                        'last 2 iOS versions',
+                        'not iOS < 10.3',
+                        'last 2 Firefox versions',
+                        'not Firefox < 54',
+                        'last 2 Edge versions',
+                        'not Edge < 15',
+                      ],
+                    },
+                    useBuiltIns: false,
+                  },
+                ],
+              ],
+              plugins: ['@babel/plugin-proposal-class-properties'],
+            },
           },
-        },
-      ],
-    }],
+        ],
+      },
+    ],
   },
 };
-
