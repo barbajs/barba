c84887ce296b28df50ae32b55d8435d2acf238ff
Luigi De Rosa <lurukee@gmail.com>
2019-07-01 13:01:09 +0200

    Feature to feature order and direction

--- themes/barba/source/js/transitions/featureToFeature.js
+++ themes/barba/source/js/transitions/featureToFeature.js
@@ -3,6 +3,33 @@ import {
   TimelineMax
 } from 'gsap/TweenMax';
 
+const NAMESPACES_ORDER = [
+  'dependency-free',
+  'small-size',
+  'core',
+  'router',
+  'prefetch',
+  'change-log',
+];
+
+// Return true for forward, false for backwards
+function isForward(fromNamespace, toNamespace) {
+  const oldIndex = NAMESPACES_ORDER.indexOf(fromNamespace);
+  const newIndex = NAMESPACES_ORDER.indexOf(toNamespace);
+
+  // From last to first
+  if (oldIndex === NAMESPACES_ORDER.length - 1 && newIndex === 0) {
+    return true;
+  }
+
+  // From first to last
+  if (oldIndex === 0 && newIndex === NAMESPACES_ORDER.length - 1) {
+    return false;
+  }
+
+  return oldIndex < newIndex ? true : false;
+}
+
 export default {
   from: {
     route: 'feature',
@@ -15,6 +42,8 @@ export default {
 
   leave(data) {
     return new Promise(resolve => {
+      const goingForward = isForward(data.current.namespace, data.next.namespace);
+
       // Logo (remove big letter)
       const container = data.current.container.querySelector('.feature__container');
       const box = data.current.container.querySelector('.feature__box');
@@ -30,19 +59,14 @@ export default {
       });
 
       tl.to(container, 1.8, {
-        x: -window.innerWidth * 1,
+        x: goingForward ? -window.innerWidth : window.innerWidth,
         ease: 'Power4.easeInOut',
       }, 0);
 
       box && tl.to(box, 1.8, {
-        x: -window.innerWidth * 0.3,
+        x: goingForward ? (-window.innerWidth * 0.3) : (window.innerWidth * 0.3),
         ease: 'Power4.easeInOut',
       }, 0);
-
-      // shapes && tl.to(shapes, 2, {
-      //   x: -(window.innerWidth * 2),
-      //   ease: 'Power4.easeInOut',
-      // }, 0);
 
       tl.to(logo, 1.5, {
         opacity: 0,
@@ -54,6 +78,8 @@ export default {
 
   enter(data) {
     return new Promise(resolve => {
+      const goingForward = isForward(data.current.namespace, data.next.namespace);
+
       const container = data.next.container.querySelector('.feature__container');
       const box = data.next.container.querySelector('.feature__box');
       const shapes = data.next.container.querySelector('.logo .big');
@@ -66,129 +92,20 @@ export default {
       });
 
       tl.from(container, 1.8, {
-        x: window.innerWidth * 1,
+        x: goingForward ? window.innerWidth : -window.innerWidth,
         ease: 'Power4.easeInOut',
       }, 0);
 
       box && tl.from(box, 2, {
-        x: window.innerWidth * 0.5,
+        x: goingForward ? window.innerWidth * 0.5 : -window.innerWidth * 0.5,
         ease: 'Power4.easeInOut',
       }, 0);
-
-      // shapes && tl.from(shapes, 2, {
-      //   x: window.innerWidth * 2,
-      //   ease: 'Power4.easeInOut',
-      // }, 0);
 
       tl.from(logo, 1.5, {
         opacity: 0,
         ease: 'Power4.easeInOut'
       }, 0);
 
-      // const bodymovin = data.next.container.querySelector('.feature__bodymovin');
-
-      // // Logo (small letter go big)
-      // const logoElementHover = data.next.container.querySelector('.hover');
-      // const logoElementBig = data.next.container.querySelectorAll('.select-to-animate');
-
-      // // TweenMax.to(bodymovin, 2, {
-      // //   opacity: 1,
-      // // });
-      // TweenMax.to(logoElementHover, 0.4, {
-      //   opacity: 1,
-      // });
-      // TweenMax.to(logoElementBig, 0.4, {
-      //   opacity: 1,
-      //   onComplete: () => {
-      //     logoElementBig.forEach(item => {
-      //       item.classList.add('to-animate');
-      //     });
-      //   },
-      // });
     });
   },
 };
-
-// import {
-//   TweenMax
-// } from 'gsap/TweenMax';
-
-// export default {
-//   from: {
-//     route: 'feature',
-//   },
-//   to: {
-//     route: 'feature',
-//   },
-
-//   leave(data) {
-//     return new Promise(resolve => {
-//       // Logo (remove big letter)
-//       const logoElementHover = data.current.container.querySelector('.hover');
-//       const logoElementBig = data.current.container.querySelectorAll('.to-animate');
-
-//       logoElementBig.forEach(item => {
-//         item.classList.remove('to-animate');
-//       });
-
-//       TweenMax.to(logoElementBig, 0.4, {
-//         onComplete: () => {
-//           TweenMax.to(logoElementBig, 0.4, {
-//             opacity: 0,
-//           });
-//           TweenMax.to(logoElementHover, 0.4, {
-//             opacity: 0,
-//             onComplete: () => {
-//               resolve();
-//             },
-//           });
-//         },
-//       });
-
-//       // Logo (prepare small letter for going big)
-//       const logoElementHoverNext = data.next.container.querySelector('.hover');
-//       const logoElementBigNext = data.next.container.querySelectorAll('.to-animate');
-//       // const bodymovin = data.next.container.querySelector('.feature__bodymovin');
-
-//       logoElementBigNext.forEach(item => {
-//         item.classList.remove('to-animate');
-//       });
-//       TweenMax.set(logoElementBigNext, {
-//         opacity: 0,
-//       });
-//       // TweenMax.set(bodymovin, {
-//       //   opacity: 0,
-//       // });
-//       TweenMax.set(logoElementHoverNext, {
-//         opacity: 0,
-//       });
-//     });
-//   },
-
-//   enter(data) {
-//     return new Promise(resolve => {
-//       resolve();
-
-//       // const bodymovin = data.next.container.querySelector('.feature__bodymovin');
-
-//       // Logo (small letter go big)
-//       const logoElementHover = data.next.container.querySelector('.hover');
-//       const logoElementBig = data.next.container.querySelectorAll('.select-to-animate');
-
-//       // TweenMax.to(bodymovin, 2, {
-//       //   opacity: 1,
-//       // });
-//       TweenMax.to(logoElementHover, 0.4, {
-//         opacity: 1,
-//       });
-//       TweenMax.to(logoElementBig, 0.4, {
-//         opacity: 1,
-//         onComplete: () => {
-//           logoElementBig.forEach(item => {
-//             item.classList.add('to-animate');
-//           });
-//         },
-//       });
-//     });
-//   },
-// };
