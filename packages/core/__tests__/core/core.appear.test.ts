fc17cc064d5a49213d2f9a9aeb9a226853727c94
thierry <thmichel@gmail.com>
2019-08-01 18:02:38 +0200

    test(core): :white_check_mark: update core.appear test

--- packages/core/__tests__/core/core.appear.test.ts
+++ packages/core/__tests__/core/core.appear.test.ts
@@ -1,20 +1,27 @@
 /* tslint:disable:no-empty */
 import { init } from '../../__mocks__/barba';
 import barba from '../../src';
+import { ITransitionData } from '../../src/defs';
+import { Logger } from '../../src/modules/Logger';
 
 init();
+
+const data: ITransitionData = {
+  ...barba.data,
+  trigger: 'barba',
+};
 
 afterEach(() => {
   barba.destroy();
 });
 
-it('do appear', () => {
-  barba.init({
-    transitions: [{ appear() {} }],
-  });
+it('do appear', async () => {
+  const t = { appear() {} };
   const spyAppear = jest.spyOn(barba.transitions, 'doAppear');
 
-  barba.appear();
+  barba.transitions.store.add('transition', t);
+
+  await barba.appear(data);
 
   expect(spyAppear).toHaveBeenCalledTimes(1);
   spyAppear.mockRestore();
@@ -24,21 +31,18 @@ it('catches error', async () => {
   expect.assertions(2);
   const errorAppear = new Error('Appear error');
   const errorTransition = new Error('Transition error [appear]');
+  const t = {
+    appear() {
+      throw errorAppear;
+    },
+  };
+  barba.transitions.store.add('transition', t);
+  Logger.setLevel('error');
 
-  barba.init({
-    logLevel: 'error',
-    transitions: [
-      {
-        appear() {
-          throw errorAppear;
-        },
-      },
-    ],
-  });
   barba.logger.error = jest.fn();
   barba.transitions.logger.error = jest.fn();
 
-  await barba.appear();
+  await barba.appear(data);
 
   expect(barba.transitions.logger.error).toHaveBeenCalledWith(errorAppear);
   expect(barba.logger.error).toHaveBeenCalledWith(errorTransition);
