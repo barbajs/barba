baabea4bf020e2d1736d10ce50a06e85ab33de02
Thierry Michel <thmichel@gmail.com>
2019-11-06 09:29:07 +0100

    test(core): :mute: remove log

--- packages/core/__tests__/hooks/hooks.test.ts
+++ packages/core/__tests__/hooks/hooks.test.ts
@@ -51,9 +51,6 @@ it('do registered hooks', async () => {
 
   hooks[hookName](fn);
   hooks[hookName](fn2);
-  hooks.beforeEnter(data => {
-    console.info(data);
-  });
 
   await hooks.do(hookName);
 
