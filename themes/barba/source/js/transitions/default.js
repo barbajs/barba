469fb0f65b0e428d45ac70424bcf6021684acee4
Thomas Claude <claude.thomas@outlook.be>
2020-01-07 08:58:42 +0100

    :construction: working on default transition

--- themes/barba/source/js/transitions/default.js
+++ themes/barba/source/js/transitions/default.js
@@ -1,26 +1,92 @@
-import { gsap } from 'gsap'
+import {
+  gsap,
+} from 'gsap'
 
 export default {
-  leave(data) {
-    return gsap
-      .to(data.current.container, {
-        duration: 0.4,
-        opacity: 0,
-        ease: 'power4.in',
-        onComplete: () => {
-          data.current.container.style.display = 'none'
-        },
-      })
-      .then()
+  leave({
+    current,
+    next,
+  }) {
+    // const transitionTitle = document.querySelector('.transition__title');
+    // const transitionBackground = document.querySelector('.transition__background');
+
+    // console.log('transitionBackground', transitionBackground);
+
+    // transitionTitle.innerHTML = next.container.dataset.barbaNamespace;
+
+    // document.body.scrollTop = 0
+    // document.documentElement.scrollTop = 0
+
+    // return gsap
+    //   .timeline()
+    //   // .to(current.container, {
+    //   //   duration: 0.4,
+    //   //   opacity: 0,
+    //   //   ease: 'power4.in',
+    //   //   onComplete: () => {
+    //   //     current.container.style.display = 'none'
+    //   //   },
+    //   // })
+    //   .to(transitionBackground, {
+    //     duration: 0.7,
+    //     x: '0',
+    //     ease: 'power4',
+    //   })
+    //   .from(next.container, {
+    //     duration: 0.1,
+    //     opacity: 0,
+    //     ease: 'power4',
+    //   })
+    //   .to(transitionBackground, {
+    //     duration: 0.7,
+    //     x: '100%',
+    //     ease: 'power4.inOut',
+    //   })
+    //   .then()
   },
-  enter(data) {
+  enter({
+    current,
+    next,
+  }) {
+    document.body.scrollTop = 0
+    document.documentElement.scrollTop = 0
+
+    const transitionTitle = document.querySelector('.transition__title');
+    const transitionBackground = document.querySelector('.transition__background');
+
+    console.log('transitionBackground', transitionBackground);
+
+    transitionTitle.innerHTML = next.container.dataset.barbaNamespace;
+
     document.body.scrollTop = 0
     document.documentElement.scrollTop = 0
 
     return gsap
-      .from(data.next.container, {
+      .timeline()
+      // .to(current.container, {
+      //   duration: 0.4,
+      //   opacity: 0,
+      //   ease: 'power4.in',
+      //   onComplete: () => {
+      //     current.container.style.display = 'none'
+      //   },
+      // })
+      .to(transitionBackground, {
         duration: 0.7,
+        x: '0',
+        ease: 'power4',
+        onComplete: () => {
+          current.container.style.display = 'none'
+        },
+      })
+      .from(next.container, {
+        duration: 0.1,
         opacity: 0,
+        ease: 'power4',
+      })
+      .to(transitionBackground, {
+        duration: 0.7,
+        x: '100%',
         ease: 'power4.inOut',
       })
       .then()
