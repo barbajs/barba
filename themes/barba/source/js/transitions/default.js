757edaf138dc89df9d7fe8eae7d9660df170bb12
Thomas Claude <claude.thomas@outlook.be>
2020-01-09 17:43:30 +0100

    :fire: useless code

--- themes/barba/source/js/transitions/default.js
+++ themes/barba/source/js/transitions/default.js
@@ -17,66 +17,56 @@ export default {
     document.body.scrollTop = 0
     document.documentElement.scrollTop = 0
 
-    return (
-      gsap
-        .timeline({
-          onComplete: () => {
-            transitionTitle.innerHTML = ''
-          },
-        })
-        // .to(current.container, {
-        //   duration: 0.4,
-        //   opacity: 0,
-        //   ease: 'power4.in',
-        //   onComplete: () => {
-        //     current.container.style.display = 'none'
-        //   },
-        // })
-        .set(transitionBackground, { clearProps: 'all' })
-        .set(transitionTitle, { y: 100 })
-        .to(transitionBackground, {
+    return gsap
+      .timeline({
+        onComplete: () => {
+          transitionTitle.innerHTML = ''
+        },
+      })
+      .set(transitionBackground, { clearProps: 'all' })
+      .set(transitionTitle, { y: 100 })
+      .to(transitionBackground, {
+        duration: 0.7,
+        x: '0',
+        ease: 'power4',
+        onComplete: () => {
+          current.container.style.display = 'none'
+        },
+      })
+      .to(
+        transitionTitle,
+        0.5,
+        {
+          y: 0,
+          opacity: 1,
+          ease: 'power4',
+        },
+        0.1
+      )
+      .from(next.container, {
+        duration: 0.1,
+        opacity: 0,
+        ease: 'power4',
+      })
+      .to(
+        transitionBackground,
+        {
           duration: 0.7,
-          x: '0',
-          ease: 'power4',
-          onComplete: () => {
-            current.container.style.display = 'none'
-          },
-        })
-        .to(
-          transitionTitle,
-          0.5,
-          {
-            y: 0,
-            opacity: 1,
-            ease: 'power4',
-          },
-          0.1
-        )
-        .from(next.container, {
-          duration: 0.1,
+          x: '100%',
+          ease: 'power4.inOut',
+        },
+        1
+      )
+      .to(
+        transitionTitle,
+        0.7,
+        {
+          y: -100,
           opacity: 0,
-          ease: 'power4',
-        })
-        .to(
-          transitionBackground,
-          {
-            duration: 0.7,
-            x: '100%',
-            ease: 'power4.inOut',
-          },
-          1
-        )
-        .to(
-          transitionTitle,
-          0.7,
-          {
-            y: -100,
-            opacity: 0,
-            ease: 'power4.inOut',
-          },
-          0.8
-        )
-        .then()
-    )
+          ease: 'power4.inOut',
+        },
+        0.8
+      )
+      .then()
   },
 }
--- themes/barba/source/js/transitions/onceFeature.js
+++ themes/barba/source/js/transitions/onceFeature.js
@@ -1,32 +1,23 @@
-import {
-  getInstance,
-} from '../app'
-import {
-  gsap,
-} from 'gsap'
+import { getInstance } from '../app'
+import { gsap } from 'gsap'
 
 export default {
   to: {
     namespace: 'feature',
   },
-  once: ({
-    next,
-  }) => {
-    const {
-      container,
-    } = next
+  once: ({ next }) => {
+    const { container } = next
     const nextFeatureSlug = container.querySelector('.feature').dataset
       .featureSlug
     const featureContainer = container.querySelector('.feature-outer')
     const featureBox = container.querySelector('.feature__box')
     const featureInstance = getInstance(container, 'feature')
-    console.log(getInstance);
-
     const tl = gsap.timeline()
 
     featureBox &&
       tl.from(
-        featureBox, {
+        featureBox,
+        {
           duration: 1.5,
           y: 100,
           opacity: 0,
@@ -36,7 +27,8 @@ export default {
       )
 
     tl.from(
-      featureContainer, {
+      featureContainer,
+      {
         duration: 1,
         y: 100,
         opacity: 0,
