130fcd983fc53b214144b19c759c718fb6599aad
Thierry Michel <thmichel@gmail.com>
2020-02-04 16:44:54 +0100

    :bookmark: v2 release

--- themes/barba/source/_js/transitions/routes.js
+++ themes/barba/source/_js/transitions/routes.js
@@ -1,21 +1,18 @@
-const { hostname } = window.location
-const root = hostname === 'barba.js.org' ? '/preview/v2/' : '/'
-
 export const getRoutes = () => [
   {
-    path: `${root}features/:type`,
+    path: '/features/:type',
     name: 'feature',
   },
   {
-    path: `${root}docs/:section/:subsection`,
+    path: '/docs/:section/:subsection',
     name: 'docs',
   },
   {
-    path: `${root}:page`,
+    path: '/:page',
     name: 'page',
   },
   {
-    path: `${root}`,
+    path: '/',
     name: 'home',
   },
 ]
