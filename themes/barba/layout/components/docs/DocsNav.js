739dc203d5f0a50366d9407c594729276b28b7e7
Xavier Foucrier <xavier.foucrier@gmail.com>
2020-04-16 23:58:47 +0200

    Improve the docs navigation with an accordion

--- themes/barba/layout/components/docs/DocsNav.js
+++ themes/barba/layout/components/docs/DocsNav.js
@@ -23,7 +23,34 @@ export default class DocsNav extends Component {
     this.close()
   }
 
-  onClick() {
+  onClick(event) {
+    const title = event.target;
+
+    // Manage navigation titles
+    if (title.classList.contains('docs__nav__title')) {
+      document.querySelectorAll('.docs__nav__list .is-open').forEach(item => {
+        item.classList.remove('is-open');
+      });
+
+      // Open the clicked title, sublist and first sublist title
+      const sublist = title.parentNode.parentNode.querySelector('.docs__nav__sublist');
+
+      title.classList.add('is-open');
+      sublist.classList.add('is-open');
+      sublist.querySelector('li:first-child').classList.add('is-open');
+      sublist.querySelector('li:first-child .docs__nav__subtitle').classList.add('is-open');
+    }
+
+    // Manage navigation subtitles
+    if (title.classList.contains('docs__nav__subtitle')) {
+      document.querySelectorAll('.docs__nav__sublist .is-open').forEach(item => {
+        item.classList.remove('is-open');
+      });
+
+      title.classList.add('is-open');
+      title.parentNode.parentNode.classList.add('is-open');
+    }
+
     this.emit('link:close')
     this.close()
   }
