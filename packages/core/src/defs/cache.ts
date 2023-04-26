9b0fd43149040182ca616a0855cd2b8c90bdf048
Xavier Foucrier <xavier.foucrier@gmail.com>
2023-04-26 15:47:49 +0200

    feat(core): :sparkles: add `CacheStatus` to reflect Promise status

--- packages/core/src/defs/cache.ts
+++ packages/core/src/defs/cache.ts
@@ -5,6 +5,8 @@
 export interface ICacheData {
   action?: CacheAction;
   request?: CacheRequest;
+  status?: CacheStatus;
 }
 export type CacheRequest = Promise<string | void>;
 export type CacheAction = 'init' | 'enter' | 'click' | 'prefetch';
+export type CacheStatus = 'pending' | 'fulfilled' | 'rejected';
