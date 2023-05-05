79e207b4e7ff88c39a33f963d3833cfb9c2ba50b
Xavier Foucrier <xavier.foucrier@gmail.com>
2023-05-05 15:46:52 +0200

    fix(core): ✅ fix request tests

--- packages/core/__tests__/utils/request.test.ts
+++ packages/core/__tests__/utils/request.test.ts
@@ -30,7 +30,7 @@ it('set correct headers', async () => {
     return res.status(200);
   });
 
-  await request(url, 2e3, requestError, barba.cache);
+  await request(url, 2e3, requestError, barba.cache, barba.headers);
 });
 
 it('throws fetch error', async () => {
@@ -39,7 +39,7 @@ it('throws fetch error', async () => {
   xhrMock.get(url, () => Promise.reject(error));
   xhrMock.error(() => {}); // tslint:disable-line:no-empty
 
-  await expect(request(url, 2e3, requestError, barba.cache)).rejects.toEqual(error);
+  await expect(request(url, 2e3, requestError, barba.cache, barba.headers)).rejects.toEqual(error);
   expect(requestError).toHaveBeenCalledWith(url, error);
   expect(barba.cache.getStatus(url)).toEqual('rejected');
 });
@@ -52,7 +52,7 @@ it('throws result error with 404', async () => {
 
   xhrMock.get(url, (req, res) => res.status(404).reason('Not found'));
 
-  await expect(request(url, 2e3, requestError, barba.cache)).rejects.toEqual(error);
+  await expect(request(url, 2e3, requestError, barba.cache, barba.headers)).rejects.toEqual(error);
   expect(requestError).toHaveBeenCalledWith(url, error);
   expect(barba.cache.getStatus(url)).toEqual('rejected');
 });
@@ -62,7 +62,7 @@ it('throws timeout error', async () => {
 
   xhrMock.get(url, () => new Promise(() => {})); // tslint:disable-line:no-empty
 
-  await expect(request(url, 100, requestError, barba.cache)).rejects.toEqual(error);
+  await expect(request(url, 100, requestError, barba.cache, barba.headers)).rejects.toEqual(error);
   expect(requestError).toHaveBeenCalledWith(url, error);
   expect(barba.cache.getStatus(url)).toEqual('rejected');
 }, 1000);
@@ -70,7 +70,7 @@ it('throws timeout error', async () => {
 it('fetch text content', async () => {
   xhrMock.get(url, (req, res) => res.status(200).body('content'));
 
-  await expect(request(url, undefined, requestError, barba.cache)).resolves.toBe('content');
+  await expect(request(url, undefined, requestError, barba.cache, barba.headers)).resolves.toBe('content');
   // expect((global as any).window.clearTimeout).toHaveBeenCalledTimes(1);
   expect(requestError).not.toHaveBeenCalled();
   expect(barba.cache.getStatus(url)).toEqual('fulfilled');
