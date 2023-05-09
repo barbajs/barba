d3acdb8dec3a494b5a17643312f778921155d22e
Xavier Foucrier <xavier.foucrier@gmail.com>
2023-05-09 10:57:03 +0200

    feat(core): :sparkles: add `IDomSibling` interface

--- packages/core/src/defs/dom.ts
+++ packages/core/src/defs/dom.ts
@@ -7,3 +7,9 @@ export type LinkEvent = MouseEvent | TouchEvent;
 export type Scope = HTMLElement | HTMLDocument;
 export type Trigger = Link | 'barba' | 'popstate' | 'back' | 'forward';
 export type Wrapper = HTMLElement | null;
+
+export interface IDomSibling {
+  before?: Element;
+  after?: Element;
+  parent?: Element;
+}
